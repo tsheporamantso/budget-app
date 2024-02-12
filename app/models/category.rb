d1a4848d1de8cd2ee6ebb9b0390647e8df3bc150
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-12 12:31:07 +0200

    Add category validations and associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_many :categories_expenses, dependent: :destroy
+  has_many :expenses, through: :categories_expenses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
 end
