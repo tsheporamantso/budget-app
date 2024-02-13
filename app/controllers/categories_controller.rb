e1314b872e7e7ebe3b4ad596c5ecb9f8b0631d7e
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-13 09:00:39 +0200

    Define new and create actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,8 +5,27 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to user_categories_path(@user)
+      flash[:success] = 'Category created successfully'
+    else
+      render :new
+    end
+  end
+
 
   private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 
   def set_user
     @user = User.find(params[:id])
