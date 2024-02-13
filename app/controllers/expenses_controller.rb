bf6622ad66066180a751a201603c84e106c7bbe4
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-13 12:26:15 +0200

    Define expenses index action and view

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,7 @@
 class ExpensesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @category = Category.find(params[:category_id])
+    @expenses = @user.expenses.includes(:category).where(category: @category)
   end
 end
