61212246cd5bc6391fa2bbf1bf19405a3c13cce5
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-13 08:46:09 +0200

    Define private action to set user and utilise call back helper

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,15 @@
 class CategoriesController < ApplicationController
+  before_action :set_user
   
   def index
     @categories = Category.all
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
